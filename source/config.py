0f1aaf8f36f4e0124b6a8168b6e0eafa2b6aa095
behnazak <barzani@seas.upenn.edu>
2019-04-22 11:51:01 -0700

    changes

---
+++ source/config.py
@@ -0,0 +1,4 @@
+LOCAL_TEMP_PATH = 'D:\\AS\\gp-structure-search\\source\\' #DIRECTORY WHERE TEMP FILES ARE
+LOCAL_MATLAB = 'C:\\Users\\bearzani\\Documents\\bin\\matlab' #path to matlab
+LOCATION = 'local'
+LOCAL_GPML_PATH = 'D:\\AS\\gp-structure-search\\source\\gpml'
